ed724dc7cd41451ccd34cf8583cac1924b773f1b
Gene Grilli <gene@grilli.us>
2017-05-09 10:13:33 -0500

    latest updates

--- homepage/views.py
+++ homepage/views.py
@@ -2,3 +2,7 @@ from django.template.response import TemplateResponse
 def homepage (request):
   return TemplateResponse(request, 'homepage.html', {})
 # Create your views here.
+def photos (request):
+  return TemplateResponse(request, 'photos.html', {})
+def food (request):
+  return TemplateResponse(request, 'food.html', {})
